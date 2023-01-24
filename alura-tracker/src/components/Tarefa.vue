97d2ea13e51c69c454530a5c22291bb9fb585d59
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2023-01-24 14:04:07 -0300

    completando uma tarefa 🐱‍💻

---
+++ alura-tracker/src/components/Tarefa.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <div class="columns">
+            <div class="column is-7">
+                Descrição da Tarefa
+            </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="15" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Cronometro from '@/components/Cronometro.vue'
+
+export default defineComponent({
+    name: "Tarefa",
+    components: {
+        Cronometro
+    },
+})
+</script>
+
+<style scoped>
+.box {
+    background: #faf0ca
+}
+</style>
