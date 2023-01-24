6c5b939352ea2d92a816ac0e8e562e5cdff03aec
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2023-01-24 13:44:09 -0300

    transformando o botão em componente 🐱‍💻

--- alura-tracker/src/components/Temporizador.vue
+++ alura-tracker/src/components/Temporizador.vue
@@ -1,30 +1,22 @@
 <template>
-    <div class="is-flex is-align-items-center is-justify-content-space-between">
-                <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-            <button class="button" @click="iniciar" :disabled="cronometroRodando">
-                <span class="icon">
-                    <i class="fas fa-play"></i>
-                </span>
-                <span>play</span>
-            </button>
-            <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-                <span class="icon">
-                    <i class="fas fa-stop"></i>
-                </span>
-                <span>stop</span>
-            </button>
-    </div>
+    <section class="is-flex is-align-items-center is-justify-content-space-between">
+      <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+      <Botao @clicado="iniciar" icone="fas fa-play" texto="play" :desabilitado="cronometroRodando" />
+      <Botao @clicado="finalizar" icone="fas fa-stop" texto="stop" :desabilitado="!cronometroRodando" />
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Cronometro from "@/components/Cronometro.vue";
+import Botao from "@/components/Botao.vue";
 
 export default defineComponent({
   name: "Temporizador",
   emits: ['aoTemporizadorFinalizado'],
   components: { 
-        Cronometro
+        Cronometro,
+        Botao
     },
   data() {
     return {
