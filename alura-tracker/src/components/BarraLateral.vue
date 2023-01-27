6a3f10c81bb1b69a8c46dcd2934f1791e9d68f93
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2023-01-27 00:13:25 -0300

    modificando o estado 🐱‍💻

--- alura-tracker/src/components/BarraLateral.vue
+++ alura-tracker/src/components/BarraLateral.vue
@@ -1,54 +1,82 @@
 <template>
-    <header>
-        <h1>
-            <img src="@/assets/logo.png" alt="">
-        </h1>
-        <button class="button" @click="alterarTema">
-            {{ textoBotao }}
-        </button>
-    </header>
+  <header>
+    <h1>
+      <img alt="Alura Tracker" src="@/assets/logo.png" />
+    </h1>
+      <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'BarraLateral',
-    emits: ['aoTemaAlterado'],
-    data() {
-        return {
-            modoEscuroAtivo: false
-        }
+  name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
+    return {
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    textoBotao() {
+      if (this.modoEscuroAtivo) {
+        return "Desativar modo escuro";
+      }
+      return "Ativar modo escuro";
     },
-    computed: {
-        textoBotao() {
-            if (this.modoEscuroAtivo) {
-                return 'Desativar modo escuro'
-            }
-            return 'Ativar modo escuro'
-        }
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
     },
-    methods: {
-        alterarTema() {
-            this.modoEscuroAtivo = !this.modoEscuroAtivo
-            this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
-        }
-    }
-})
+  },
+});
 </script>
 
 <style scoped>
 header {
-    padding: 1rem;
-    background: #0d3b66;
-    width: 100%;
-    height: 100vh;
-    text-align: center;
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
 }
+
 @media only screen and (max-width: 768px) {
-    header {
-        padding: 2.5rem;
-        height: auto;
-    }
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+
+.panel li {
+    margin: 8px 0;
+}
+.link {
+    color: #fff;
+}
+.link:hover {
+    color: #faf0ca;
+}
+.link.router-link-active {
+    color: #faf0ca;
 }
 </style>
