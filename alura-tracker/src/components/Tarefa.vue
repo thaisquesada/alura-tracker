60fded52c3235a39f8f0c8451b903274dddf4865
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2023-01-30 15:27:44 -0300

    computed props 🐱‍💻

--- alura-tracker/src/components/Tarefa.vue
+++ alura-tracker/src/components/Tarefa.vue
@@ -1,31 +1,36 @@
 <template>
-    <Box>
-        <div class="columns">
-            <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
-            <div class="column">
-                <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
-            </div> 
-        </div>
-    </Box>
+  <Box>
+    <div class="columns">
+      <div class="column is-4">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
+      <div class="column is-3">
+        {{ tarefa.projeto }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+      </div>
+    </div>
+  </Box>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import Cronometro from '@/components/Cronometro.vue'
-import ITarefa from '@/interfaces/ITarefa'
-import Box from '@/components/Box.vue'
+import { defineComponent, PropType } from "vue";
+import Cronometro from "@/components/Cronometro.vue";
+import ITarefa from "@/interfaces/ITarefa";
+import Box from "@/components/Box.vue";
 
 export default defineComponent({
-    name: "Tarefa",
-    components: {
-        Box,
-        Cronometro
+  name: "Tarefa",
+  components: {
+    Box,
+    Cronometro,
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
     },
-    props: {
-        tarefa: {
-            type: Object as PropType<ITarefa>,
-            required: true
-        }
-    }
-})
+  },
+});
 </script>
