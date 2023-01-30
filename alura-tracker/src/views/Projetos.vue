611386f95aa0bde9640dc47e512562c84a658c85
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2023-01-30 11:10:08 -0300

    editando um projeto 🐱‍💻

--- alura-tracker/src/views/Projetos.vue
+++ alura-tracker/src/views/Projetos.vue
@@ -1,31 +1,33 @@
 <template>
   <section class="projetos">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeProjeto" class="label"> Nome do Projeto </label>
-        <input
-          type="text"
-          class="input"
-          v-model="nomeProjeto"
-          id="nomeProjeto"
-        />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
+    <router-link to="/projetos/novo" class="button">
+    <span class="icon is-small">
+      <i class="fas fa-plus"></i>
+    </span>
+    <span>Novo projeto</span>
+    </router-link>
     <table class="table is-fullwidth">
       <thead>
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>
+            Ações
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" :key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+            <span class="icon is-small">
+              <i class="fas fa-pencil-alt"></i>
+            </span>
+            </router-link>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -38,21 +40,9 @@ import { useStore } from "@/store";
 
 export default defineComponent({
   name: "Projetos",
-  data() {
-    return {
-      nomeProjeto: "",
-    };
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeProjeto)
-      this.nomeProjeto = "";
-    },
-  },
   setup() {
     const store = useStore()
     return {
-      store,
       projetos: computed(() => store.state.projetos)
     }
   }
