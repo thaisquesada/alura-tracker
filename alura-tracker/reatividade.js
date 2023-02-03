3c8b1c4d3c46db9cb2070d486cb29d51d205a2d5
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2023-02-03 14:45:15 -0300

    proxy 🐱‍💻

---
+++ alura-tracker/reatividade.js
@@ -0,0 +1,19 @@
+const projeto = {
+    id: 1,
+    descricao: 'Alura Tracker 3.0'
+}
+
+const proxy = new Proxy(projeto, {
+    get(objetoOriginal, chave) {
+        console.log(`Alguém pediu a chave ${chave} do projeto`)
+        return objetoOriginal[chave]
+    },
+    set(objetoOriginal, chave, valor) {
+        console.log(`Alguém alterou a chave ${chave} do projeto para o valor ${valor}`)
+        objetoOriginal[chave] = valor
+    }
+})
+
+proxy.descricao = 'Reatividade é muito legal'
+
+console.log(proxy.descricao)
